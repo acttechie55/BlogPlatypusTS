aaf8ef80f537d1639edf16a738304015b0596b2c
acttechie55 <actaylor55@gmail.com>
2015-10-13 14:39:12 -0500

    first commit

---
+++ app/src/viewcontrols/home/home.vc.ts
@@ -0,0 +1,11 @@
+//This is the controller for my homepage//
+import {register} from 'platypus';
+import BaseViewControl from '../base/base.vc';
+
+export default class HomeViewControl extends BaseViewControl {
+    templateString: string = require('./home.vc.html');
+
+    context: any = {};
+}
+
+register.viewControl('home-vc', HomeViewControl);
